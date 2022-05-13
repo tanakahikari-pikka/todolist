e00abbc9db4355c6feb854bfb6c81ab0206acfb0
EC2 Default User <ec2-user@ip-172-31-23-73.ec2.internal>
2022-05-13 08:57:33 +0000

    added new colomn

--- db/migrate/20220513084747_create_lists.rb
+++ db/migrate/20220513084747_create_lists.rb
@@ -1,7 +1,9 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
+      # null: falseで空の状態で保存させるのを防ぐ
+      t.string :name, null: false
+      t.text :caption, null: false
       t.timestamps
     end
   end
