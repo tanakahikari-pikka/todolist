c41dca6586b1bdf21b3619079b3db6639dd70a68
EC2 Default User <ec2-user@ip-172-31-23-73.ec2.internal>
2022-05-14 07:10:27 +0000

    category new

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,8 +1,20 @@
 class CategoryController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
   def index
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_index_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -10,4 +22,9 @@ class CategoryController < ApplicationController
 
   def destroy
   end
+
+  private
+        def category_params
+            params.require(:category).permit(:name)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'category/index'
-  get 'category/create'
-  get 'category/edit'
-  get 'category/destroy'
-  get 'create/edit'
-  get 'create/show'
-  get 'create/destroy'
-  get 'list/create'
-  get 'list/edit'
-  get 'list/show'
-  get 'list/destroy'
+  resources :list, only: %i(new create edit update destroy)
+  resources :category, only: %i(new index create edit update destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
